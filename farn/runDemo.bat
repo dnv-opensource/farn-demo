aa5643b835ad110fe1650bd2e62d3b7f4efec142
Claas <claas.rostock@dnv.com>
2024-10-24 13:50:52 +0200

    farn/runDemo.bat : added commands to run also the hilbert_farnDict (in addition to the lhs_farnDict)

--- farn/runDemo.bat
+++ farn/runDemo.bat
@@ -3,3 +3,9 @@ farn sampled.lhs_farnDict --generate
 farn sampled.lhs_farnDict --execute prepare
 farn sampled.lhs_farnDict --execute run
 farn sampled.lhs_farnDict --execute post
+
+farn hilbert_farnDict --sample
+farn sampled.hilbert_farnDict --generate
+farn sampled.hilbert_farnDict --execute prepare
+farn sampled.hilbert_farnDict --execute run
+farn sampled.hilbert_farnDict --execute post
